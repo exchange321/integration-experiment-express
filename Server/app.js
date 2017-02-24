b9cf9c93b50a211e3aea114a85aeaf1026fe0428
exchange321 <chuen_hong@yahoo.com>
2017-02-24 14:44:12 +1000

    DDP Server setup

--- Server/app.js
+++ Server/app.js
@@ -1,11 +1,15 @@
 /**
  * Created by Wayuki on 24-Feb-17.
  */
+const http = require('http');
 const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
+
+const MongoStream = require('mongo-trigger');
+const DDPServer = require('ddp-server-reactive');
 
 const teacherRoute = require('./routes/teachers');
 const topicRoute = require('./routes/topics');
@@ -15,7 +19,8 @@ mongoose.Promise = require('bluebird');
 const app = express();
 
 // mongodb connection
-mongoose.connect('mongodb://localhost:27017/ie-express');
+const serverUrl = 'mongodb://localhost:27017/ie-express';
+mongoose.connect(serverUrl);
 const db = mongoose.connection;
 
 app.use(logger('dev'));
@@ -36,6 +41,30 @@ db.on('error', (err) => {
 db.once('open', () => {
     // eslint-disable-next-line no-console
     console.log('MongoDB Connected!');
+
+    // DDP Server
+    const Teacher = require('./model/teacher');
+    const Topic = require('./model/topic');
+    const Course = require('./model/course');
+
+    const updateTopic = (topics) => {
+        Topic.find().exec().then((topicData) => {
+            console.log('Updated!');
+            topics['data'] = topicData;
+        }).catch((err) => {
+            console.log(err);
+        });
+    };
+
+    const server = new DDPServer({ port: 3010 });
+    // let teachers = server.publish('teachers');
+    let topics = server.publish('topics');
+    // let courses = server.publish('courses');
+    updateTopic(topics);
+
+    const watcher = MongoStream({ format: 'pretty' });
+
+    watcher.watch('ie-express.topics', () => updateTopic(topics));
 });
 
 app.use((req, res, next) => {
--- WebApp/src/index.jsx
+++ WebApp/src/index.jsx
@@ -1,5 +1,6 @@
 /* Created by Wayuki on 07-Jan-17 0007. */
 import React from 'react';
+import DDPClient from 'ddp-client';
 
 import {Provider} from 'react-redux';
 import configureStore from './store/configureStore';
@@ -8,13 +9,33 @@ import AppRouter from './AppRouter.jsx';
 
 const store = configureStore();
 
+const ddpclient = new DDPClient({
+    host: 'localhost',
+    port: 3010,
+    ssl: false,
+    autoReconnect: true,
+    autoReconnectTimer: 500,
+    maintainCollections : true,
+    ddpVersion : '1',
+});
+
+ddpclient.connect((err, wasReconnect) => {
+    if (err) {
+        console.log('DDP Connection Error!');
+        return false;
+    }
+    console.log('DDP Connected!');
+
+    ddpclient.subscribe('topics', [], () => {
+        console.log('Topics Subscribed!');
+        console.log(ddpclient.collections.topics);
+    });
+});
+
 const AppContainer = () => (
-< Provider
-store = {store} >
-    < AppRouter
-store = {store} / >
-    < / Provider >
-)
-;
+    <Provider store = {store} >
+        <AppRouter store = {store} />
+    </Provider>
+);
 
 export default AppContainer;
